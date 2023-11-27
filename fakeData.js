4d0dc048efb89bfba851e6537387e9879ae1c2ac
amit <amit.bhavsar@techalchemy.com>
2023-11-27 19:10:52 +0530

    get all user and quote, get single user and quote by id

---
+++ fakeData.js
@@ -0,0 +1,60 @@
+
+export const users = [
+    {
+        "id": 1,
+        "name": "John Doe",
+        "email": "johndoe@example.com",
+        "role": "admin",
+        "password": "password123"
+      },
+      {
+        "id": 2,
+        "name": "Jane Doe",
+        "email": "janedoe@example.com",
+        "role": "user",
+        "password": "Pass@123"
+      },
+      {
+        "id": 3,
+        "name": "Peter Jones",
+        "email": "peterjones@example.com",
+        "role": "admin",
+        "password": "password123"
+      },
+      {
+        "id": 4,
+        "name": "Raj",
+        "email": "raj@mailinator.com",
+        "password": "Hello@1234",
+        "role": "user"
+      }
+]
+
+export const quotes = [
+  {
+    id: 1,
+    user_id: 2,
+    quote: 'The best way to predict the future is to invent it.'
+
+  },{
+    id: 2,
+    user_id: 3,
+    quote: 'If you can’t explain it simply, you don’t understand it well enough'
+  },{
+    id: 3,
+    user_id: 4,
+    quote: 'A computer weighs as much as a elephant, and yet I feel nothing when I look at one.'
+  },{
+    id: 4,
+    user_id: 1,
+    quote: 'I have not failed. I\'ve just found 10,000 ways that won\'t work.'
+  },{
+    id: 5,
+    user_id: 2,
+    quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.'
+  },{
+    id: 6,
+    user_id: 3,
+    quote: 'You miss 100% of the shots you never take.'
+  }
+]
--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { quotes, users } from "./fakeData.js";
 
 const typeDefs = gql`
   type Query {
-    greetings: String
+    greets: String
+    users: [User]
+    quotes: [Quote]
+    user(id: ID!): User
+    quote(user_id:ID!): [Quote]
+  }
+
+  type User{
+    id: ID
+    name: String
+    email: String,
+    role: String,
+    greets: String,
+    quotes: [Quote]
+  }
+
+  type Quote{
+    id:ID
+    quote: String
+    user_id: ID
   }
 `;
 const resolvers = {
   Query: {
-    greetings: () => "Hello World!",
+    greets: () => "Hello World!",
+    users: () => users,
+    quotes: () => quotes,
+    user: (_, args) => users.find((u) => u.id == args.id),
+    quote: (_, {user_id}) => quotes.filter(q => q.user_id == user_id)
   },
+  User:{
+    greets:(parent) => `Hi ${parent.name}!`,
+    quotes: (parent) => quotes.filter((q) => q.user_id === parent.id)
+  }
 };
 
 const server = new ApolloServer({
